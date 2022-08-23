044623e236750a2f61a2cb96ce0833e113921b88
Gabriel Sanches <gabriel@gsr.dev>
2022-08-23 15:42:21 -0300

    Refactor testing script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-nvim --headless --noplugin -u tests/minimal.vim -c "lua require(\"plenary.test_harness\").test_directory_command('tests {minimal_init = \"tests/minimal.vim\"}')"
+nvim \
+	--headless \
+	--noplugin \
+	-u tests/minimal.vim \
+	-c "lua require(\"plenary.test_harness\").test_directory_command('tests {minimal_init = \"tests/minimal.vim\"}')"
